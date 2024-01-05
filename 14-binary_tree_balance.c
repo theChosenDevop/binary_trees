e8802621e9f02a4ce7775898f0b1b5c4f54ba00a
Solomon Adola <77428753+solomonadola@users.noreply.github.com>
2024-01-06 00:15:16 +0300

    Task 14 issue solved

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,20 +7,18 @@
  *
  * Return: The height of the tree, 0 if tree is NULL
  */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height, right_height;
+	size_t left_height, right_height;
 
-    if (!tree)
-        return (0);
+	if (!tree)
+		return (0);
 
-    left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-    right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-
-    return (left_height > right_height ? left_height : right_height);
+	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (left_height > right_height ? left_height : right_height);
 }
-
-
 
 /**
  * binary_tree_balance - Measures the balance factor of a binary tree
@@ -29,6 +27,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
  *
  * Return: Balance factor of the tree, 0 if tree is NULL
  */
+
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	int left_height, right_height;
