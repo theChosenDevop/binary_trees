35bccef621ecbcf9d23ed8a5cc8294bfc1af6f2a
solomonadola <solomonadola84@gmail.com>
2024-01-05 12:51:11 -0800

    Task 12 fixed

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -10,16 +10,13 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	if (tree->left == NULL && tree->right == NULL)
-	{
-		return (1);
-	}
-	size_t left_leaves = binary_tree_leaves(tree->left);
-	size_t right_leaves = binary_tree_leaves(tree->right);
+	size_t leaves = 0;
 
-	return (left_leaves + right_leaves);
+	if (tree)
+	{
+		leaves += (!tree->left && !tree->right) ? 1 : 0;
+		leaves += binary_tree_leaves(tree->left);
+		leaves += binary_tree_leaves(tree->right);
+	}
+	return (leaves);
 }
