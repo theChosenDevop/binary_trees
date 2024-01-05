1855edd33fa2828ca624c41fdadff1aaa6d63700
theChosenDevop <tobbey.adesanya@gmail.com>
2024-01-04 20:06:24 -0800

    delete tree

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -7,9 +7,7 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return;
-	else
+	if (tree != NULL)
 	{
 		binary_tree_delete(tree->left);
 		binary_tree_delete(tree->right);
